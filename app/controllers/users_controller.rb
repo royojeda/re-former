d6275613808a408ba76dc83b8f52181913125c93
Dennis Ojeda <royojeda1@gmail.com>
2022-07-04 04:41:27 +0800

    Add User#edit and User#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
